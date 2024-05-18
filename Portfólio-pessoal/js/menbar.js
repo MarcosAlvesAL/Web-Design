db144a835270da7e074c584c0a380b388470ef83
MarcosAlvesAL <121587762+MarcosAlvesAL@users.noreply.github.com>
2024-05-17 23:31:08 -0300

    att

--- Portfólio-pessoal/js/menbar.js
+++ Portfólio-pessoal/js/menbar.js
@@ -1,3 +1,4 @@
+// Função Esconde/mostra nav
 var menu = document.getElementById('button_menu')
 
 function menuBar(){
@@ -7,3 +8,28 @@ function menuBar(){
     else{ (menu.classList.add('menu_open'));
     }
 };
+
+
+
+//Função retorna ao topo
+
+var mybutton = document.getElementById("topBtn");
+
+// Mostra o botão quando o usuário rolar para baixo 20px
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// Quando o usuário clicar no botão, rola a página para o topo
+function topFunction() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0;
+}
