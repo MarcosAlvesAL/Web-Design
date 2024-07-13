6831ae94628959fec5ca104b4ab749bd1e385ae9
MarcosAlvesAL <121587762+MarcosAlvesAL@users.noreply.github.com>
2024-07-12 22:43:24 -0300

    exercicio condicionais

--- JS/Aulas/Aula-01/js.-5-.js
+++ JS/Aulas/Aula-01/js.-5-.js
@@ -1,10 +1,27 @@
-let n1 = parseInt(prompt('Digite um numero de 0 a 10'));
-let n2 = parseInt(prompt('Digite outro numero de 0 a 10'));
+// Pedir para o usuario colocar dois numeros de 1 a 10
+let n1 = parseInt(prompt('Digite um numero de 1 a 10'));
+let n2 = parseInt(prompt('Digite outro numero de 1 a 10'));
 
+// verificar se o usuario colocou um numero igual a 0 ou maior que 10
 
-let media = (n1 + n2)/2
-if ((media <= 4) || (n1 || n2 == 0)){
-    alert('true');
-} else{
-    alert('false');
+if ( ( n1 > 10 || n2 > 10 ) || ( n1 == 0 || n2 == 0 ) ){
+    alert('DIGITE APENAS NUMEROS DE 1 A 10');
+} else { // se tiver tudo ok, calcular a media dos dois numeros
+    let media = (n1 + n2) / 2;
+    if (media >= 7) { // se a media for maior que 7 avisar aprovado --- se tiver menor está reprovado
+        alert('Parabéns, você foi aprovado!!')
+    } else{ 
+        alert('Que pena, estude mais.')
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
