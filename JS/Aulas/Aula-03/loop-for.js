56a5cc874c5f71abe677c0f63fd95f4c976c5603
MarcosAlvesAL <121587762+MarcosAlvesAL@users.noreply.github.com>
2024-07-16 22:35:42 -0300

    Update loop-for.js

--- JS/Aulas/Aula-03/loop-for.js
+++ JS/Aulas/Aula-03/loop-for.js
@@ -1,13 +1,25 @@
 //tabuada até o 1000
 
 // pedir para o usuario digitar um número
-let numero = 1
+ /* let numero = 1
 // verificar se realmente é um número
-if (isNaN(numero) == true){
+//if (isNaN(numero) == true){
     alert('Digite apenas números');
 } else {
     for (i=0; i<1000; i++){
         document.write('&nbsp &nbsp &nbsp &nbsp' + numero + ' x ' + i + ' = ' + numero * i + '<br>');
     }
     document.write('fim do programa');
+}
+*/
+
+
+
+// pedir para o usuario digitar um numero
+let numeroSolicitado = parseInt(prompt('Digite um número'));
+
+// somar e mostrar todos os numeros pares até o numero que o usuario digitou
+
+for (i=0; i <= numeroSolicitado; i = i + 2) {
+    console.log( i + '<br>' )
 }
