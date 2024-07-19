eb7d899abe92a0a89ffddd645ca11d6fa11723d6
MarcosAlvesAL <121587762+MarcosAlvesAL@users.noreply.github.com>
2024-07-18 22:56:43 -0300

    Pequenos desafios - for e while

---
+++ JS/Aulas/Aula-03/desafio.js
@@ -0,0 +1,13 @@
+// mostre na tela todos os anos a partir de 1004 atpe 2024, pulando de 4 em 4 anos
+
+/*
+let ano = 1004
+ while (ano <= 2024) {
+    document.write('O ano é: ' + ano + '<br>');
+    ano += 4;
+}
+*/
+
+for ( i= 1004; i <= 2024; i = i + 4 ){
+    document.write('O ano é: ' + i + '<br>');
+}
