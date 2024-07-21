4d6b3f6bd89b27cf0d786e78ff05ef93802e2a1c
MarcosAlvesAL <121587762+MarcosAlvesAL@users.noreply.github.com>
2024-07-20 21:17:27 -0300

    Update desafio.js

--- JS/Aulas/Aula-03/desafio.js
+++ JS/Aulas/Aula-03/desafio.js
@@ -7,7 +7,29 @@ let ano = 1004
     ano += 4;
 }
 */
-
+/*
 for ( i= 1004; i <= 2024; i = i + 4 ){
     document.write('O ano é: ' + i + '<br>');
 }
+    */
+
+/* jogue dois dados infinitas vezes até que a soma dos pontos dos dados seja par por 10 vezes
+e quantas vezes esses dados foram jogados */
+let dado1 = parseInt((Math.random() * 6) + 1);
+let dado2 = parseInt((Math.random() * 6) + 1);
+
+let par = 0
+let contador = 0
+while (par <= 9) {
+    console.log(dado1 = parseInt((Math.random() * 6) + 1));
+    console.log(dado2 = parseInt((Math.random() * 6) + 1));
+    console.log('-----------');
+    contador ++
+    if (dado1 == dado2){
+        par ++
+        console.log('Dados iguais! ' + dado1 + '+' + dado2)
+    } else {
+        continue
+    }
+}
+console.log('A Quantidade total de dados jogados foram: ' + contador)
