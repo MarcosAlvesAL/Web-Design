392172c9b1dd82f3b99224df470b87562898b225
MarcosAlvesAL <121587762+MarcosAlvesAL@users.noreply.github.com>
2024-07-25 22:58:54 -0300

    att

--- JS/Aulas/Aulas-Arrays/array-intro.js
+++ JS/Aulas/Aulas-Arrays/array-intro.js
@@ -1,12 +1,20 @@
 let nomes = ['Joaquim', 'Seraphina', 'Josephina', 'Larina', 'José', 'Zefa']; // Sintaxe Literal
-
+let nomeInserido = prompt('Digite um nome');
 
 // let nomes2 = new Array('Joaquim', 'Seraphina', 'Josephina', 'Larina'); // Sintaxe Função Construtora
 // console.log(nomes2[3]);
 
 
+// uma forma de acrescentar um nome no final da lista
+nomes[nomes.length] = nomeInserido;
+
+
+
 for (i = 0 ; i < nomes.length ; i ++){
-    console.log(i + ': '+ nomes[i]);
-    console.log('-----------------------------')-
+    document.write(i + ': '+ nomes[i] + '<br>');
+    document.write('-----------------------------' + '<br>')
 
 }
+
+
+document.write('total de itens no array: ' + nomes.length + '<br>')
