a9e8ac773d7c1b529e3c0f58699471600bf0a0e8
MarcosAlvesAL <121587762+MarcosAlvesAL@users.noreply.github.com>
2024-07-25 22:41:36 -0300

    introdução-arrays

---
+++ JS/Aulas/Aulas-Arrays/array-intro.js
@@ -0,0 +1,12 @@
+let nomes = ['Joaquim', 'Seraphina', 'Josephina', 'Larina', 'José', 'Zefa']; // Sintaxe Literal
+
+
+// let nomes2 = new Array('Joaquim', 'Seraphina', 'Josephina', 'Larina'); // Sintaxe Função Construtora
+// console.log(nomes2[3]);
+
+
+for (i = 0 ; i < nomes.length ; i ++){
+    console.log(i + ': '+ nomes[i]);
+    console.log('-----------------------------')-
+
+}
