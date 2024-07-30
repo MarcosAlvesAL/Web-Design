77138ead7e8b653ad7f5e6a9b5716fdccc397723
MarcosAlvesAL <121587762+MarcosAlvesAL@users.noreply.github.com>
2024-07-29 22:37:30 -0300

    att desafios arrays

---
+++ JS/Aulas/Aulas-Arrays/array-desafio-2.js
@@ -0,0 +1,27 @@
+// Dado um array de numeros, mostre num alert a soma de todos eles
+
+// Dificultando o desafio, coloque diferentes tipos de dados dentro do array
+
+// Array mista
+const arrayNumeros = [1, 4, [], 6, undefined, 12, 5, false, 7, 8, 3, null, 13, 'teste', 55, true,78];
+// Soma é o valor final que eu espero somar
+let soma = 0
+console.log('Soma Inicia em: ' + soma)
+
+// iniciando no indice zero, enquanto for menor que o tamanho do array, soma mais 1
+for (i = 0 ; i < arrayNumeros.length ; i++){
+    // nesse if, estou verificando se o tipo do objeto nessa posição da array é tipo number
+    
+    if (typeof arrayNumeros[i] === 'number'){
+        // se for tipo number, a váriavel soma vai ser somada com o valor do objeto do loop atual
+        soma += arrayNumeros[i];
+        console.log('Próximo objeto: + ' + arrayNumeros[i]);
+        console.log('Valor atual= ' + soma)
+
+    } else{ // se não for do tipo number, seguimos o loop
+        continue
+    }
+
+} 
+
+console.log('Resultado Final: ' + soma);
