c43d9858699b7eed606b8b07525258a0ee19b84b
MarcosAlvesAL <121587762+MarcosAlvesAL@users.noreply.github.com>
2024-07-31 22:40:57 -0300

    att desafios

--- JS/Aulas/Aulas-Arrays/desafio-4.js
+++ JS/Aulas/Aulas-Arrays/desafio-4.js
@@ -1,7 +1,7 @@
 // Dado um array qualquer, mostre em um alert um boolean indicando se encontrou um dos seguintes valores nesse array
 // null, undefined ou NaN
 
-/*
+
 
 const arrayQualquer = [21, NaN,541, undefined, 51,51, null, 61,];
 
@@ -16,7 +16,14 @@ for (i = 0 ; i < arrayQualquer.length ; i++){
         console.log('-------------------------')
     }
 }
-*/
+
 
 
 // Dado um array de nomes mostre na tela todos os nomes de trás pra frente
+
+
+const arrayNomes = ['Joaquina', 'Mariana', 'José', 'Felipe', 'Luiz', 'Andreia'];
+
+for (i = arrayNomes.length -1 ; i >= 0 ; i--){
+    console.log(arrayNomes[i]);
+}
