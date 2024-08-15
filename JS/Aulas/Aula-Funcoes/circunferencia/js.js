494be0ee6a69d852c943619eb71d51838168d525
MarcosAlvesAL <121587762+MarcosAlvesAL@users.noreply.github.com>
2024-08-14 22:47:14 -0300

    Update Desafio Area Circunferencia
    Desafio para ir consolidando o uso de funções para reaproveitamento de código

--- JS/Aulas/Aula-Funcoes/circunferencia/js.js
+++ JS/Aulas/Aula-Funcoes/circunferencia/js.js
@@ -1,3 +1,24 @@
 // Escreva um progrma para calcular a area de uma circunferencia
-
 //utilizando o Math.PI
+
+// solicitar ao usuario o Raio
+
+
+// Buscando as Ids e transformando em const
+const raio = document.getElementById("raio");
+const output = document.getElementById("output");
+
+// Buscando Criar o Hábito de Sempre enviar tudo pro Dom apenas uma vez, para melhorar o desempenho
+let apresentacaohtml = ""
+
+function calcularAreaCircunferencia(){
+    // raioValue vai ser igual o valor colocado pelo usuario no momento que a função foi chamada
+    let raioValue = raio.value;
+    // Aréa = PI * Raio²
+    let areaCircunferencia = Math.PI * (raioValue * raioValue);
+    // Apresentação Recebendo os valores
+    apresentacaohtml = `<div class="teste"> O Valor da Area é ${areaCircunferencia} </div>`;
+    // Enviando tudo para o Dom        
+    output.innerHTML = apresentacaohtml;
+}
+
