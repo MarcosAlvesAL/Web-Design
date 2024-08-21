2ce64a889ba714a2e331d82ebfc74f37b9103b33
MarcosAlvesAL <121587762+MarcosAlvesAL@users.noreply.github.com>
2024-08-20 22:21:17 -0300

    update

--- JS/Aulas/Aula-Funcoes/aula/js.js
+++ JS/Aulas/Aula-Funcoes/aula/js.js
@@ -1,1 +1,35 @@
+// Escreva um progrma para calcular a area de uma circunferencia
+//utilizando o Math.PI
+
+// solicitar ao usuario o Raio
+
+// Buscando as Ids e transformando em const
+const raio = document.getElementById("raio");
+const output = document.getElementById("output");
+
+let apresentacaohtml = ""
+
+
+// Essa Função serve apenas para receber os dados, calcular e mostrar na tela
+function calcularAreaCircunferencia(raioV){    
+    //raioV é o valor que vai ser passado por parametro
+    let areaCircunferencia = Math.PI * (raioV * raioV);
+    
+    apresentacaohtml = `<div class="teste"> O Valor da Area é ${areaCircunferencia} </div>`;
+    
+    output.innerHTML = apresentacaohtml;
+}
+
+function calcularAreaPrompt(){
+    // recebimento do valor do raio pelo prompt
+    let raioValue = parseFloat(prompt("Digite o Valor do Raio"));
+    // passando para a função o valor do raio
+    calcularAreaCircunferencia(raioValue)       
+}
+function calcularAreaInput(){
+    // recebimento do valor do raio via input
+    let raioValue = raio.value;
+    // passando para a função o valor do raio
+    calcularAreaCircunferencia(raioValue)    
+}
 
