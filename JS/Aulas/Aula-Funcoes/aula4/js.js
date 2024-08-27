50fda8cdc3a41ea5ae9c992f76295dd90d878a19
MarcosAlvesAL <121587762+MarcosAlvesAL@users.noreply.github.com>
2024-08-26 21:59:55 -0300

    Update js.js

--- JS/Aulas/Aula-Funcoes/aula4/js.js
+++ JS/Aulas/Aula-Funcoes/aula4/js.js
@@ -4,6 +4,7 @@ const output = document.getElementById("output");
 // Base * Largura
 function calcularAreaRetangulo(b,l){
     if(typeof b !== "number" || typeof l !== "number"){
+        // Erro personalizado
         throw Error("Essa Função calcularAreaRetangulo só aceita parametros do tipo numero");
     }
     return b * l;
@@ -14,6 +15,7 @@ function calcularAreaRetangulo(b,l){
 // Base * Largura / 2
 function calcularAreaTriangulo(b,l){
     if(typeof b !== "number" || typeof l !== "number"){
+        // Erro personalizado
         throw Error("Essa Função calcularAreaTriangulo só aceita parametros do tipo numero");
     }
     return (b * l)/2;
@@ -23,6 +25,7 @@ function calcularAreaTriangulo(b,l){
 // Pi * Raio²
 function calcularAreaCircunferencia(raio){
     if(typeof raio !== "number"){
+        // Erro personalizado
         throw Error("Essa Função calcularAreaCircunferencia só aceita parametros do tipo numero");
     }
     return Math.PI * (raio * raio);
